304f0be98ac66c6e7b233e4cc046af96f042d5fe
Runji Wang <wangrunji0408@163.com>
2020-02-05 14:18:00 +0800

    fix code format

--- src/sbi.rs
+++ src/sbi.rs
@@ -26,14 +26,14 @@ pub fn shutdown() -> ! {
 
 pub fn set_timer(stime_value: u64) {
     #[cfg(target_pointer_width = "32")]
-        sbi_call(
+    sbi_call(
         SBI_SET_TIMER,
         stime_value as usize,
         (stime_value >> 32) as usize,
         0,
     );
     #[cfg(target_pointer_width = "64")]
-        sbi_call(SBI_SET_TIMER, stime_value as usize, 0, 0);
+    sbi_call(SBI_SET_TIMER, stime_value as usize, 0, 0);
 }
 
 pub fn clear_ipi() {
